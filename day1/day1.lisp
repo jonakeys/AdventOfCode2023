2115d9fd104fb4d7b5b0d5af22c854828189ce3c
Jonathan van der Steege <jonathan@jonakeys.nl>
2023-12-01 16:17:14 +0100

    Day 1 part 1

--- day1/day1.lisp
+++ day1/day1.lisp
@@ -1,2 +1,13 @@
-(defun trebuchet()
-  )
+(defun trebuchet-part1()
+  (with-open-file (file "input.txt")
+	(let ((numbers) (sum 0))
+	  (loop for line = (read-line file nil nil)
+			while line
+			do
+			   (setq line (coerce line 'list))
+			   (setq numbers '())
+			   (dolist (x line numbers)
+				 (if (digit-char-p x)
+					 (push (digit-char-p x) numbers)))
+			   (setq sum (+ sum (* (car (last numbers)) 10) (car numbers))))
+	  sum)))
